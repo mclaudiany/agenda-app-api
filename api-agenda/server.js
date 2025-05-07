45f5a96c8a1e6d6762581de39d17903fc5318d59
mclaudiany <mp.claudiany@gmail.com>
2025-05-06 23:49:34 -0300

    Add files via upload

--- api-agenda/server.js
+++ api-agenda/server.js
@@ -3,20 +3,19 @@ const sequelize = require("./src/config/database");
 
 const PORT = process.env.PORT || 8080;
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Conectado ao banco de dados");
 
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log("Conectado ao banco de dados");
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar ao banco de dados:", err);
+  });
 
-//     app.listen(PORT, () => {
-//       console.log(`Servidor rodando na porta ${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error("Erro ao conectar ao banco de dados:", err);
-//   });
-
- app.listen(PORT, () => {
-   console.log(`Servidor rodando na porta ${PORT}`);
- });
+//  app.listen(PORT, () => {
+//    console.log(`Servidor rodando na porta ${PORT}`);
+//  });
